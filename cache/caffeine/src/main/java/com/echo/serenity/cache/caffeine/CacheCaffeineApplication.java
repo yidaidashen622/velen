5ee6a54f5708b432924bb3f3e74d070cf9194227
xushu <xushu@kuaishou.com>
2020-11-19 13:58:34 +0800

    init

---
+++ cache/caffeine/src/main/java/com/echo/serenity/cache/caffeine/CacheCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.echo.serenity.cache.caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheCaffeineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheCaffeineApplication.class, args);
+	}
+
+}
